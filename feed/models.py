f7058a2fce5914e07fd966373ff3b7be6bb9f2df
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-09 09:55:53 +0530

    usual code track

--- feed/models.py
+++ feed/models.py
@@ -18,3 +18,24 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.user.username}'s post at {self.created_at}"
     
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
+    
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)    
+
+    def __str__(self):
+        return f"{self.user.username} commented on Post {self.post.id}"
--- feed/urls.py
+++ feed/urls.py
@@ -3,7 +3,9 @@ from .views import (
     PostDetailView,
     FeedView,
     PostListCreateView,
-    PostUpdateView
+    PostUpdateView,
+    CommentListCreateView,
+    CommentDetailView
 )
 
 
@@ -15,15 +17,15 @@ urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
      # List all posts / create new
     path("posts/", PostListCreateView.as_view(), name="post-list-create"),
-
     # Retrieve / delete a post by owner
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-
     # Update post by owner
     path("posts/<int:post_id>/update/", PostUpdateView.as_view(), name="post-update"),
-
     # Feed - posts from people you follow
     path("feed/", FeedView.as_view(), name="feed"),
-         
+    
+    path("posts/<int:post_id>/comments/", CommentListCreateView.as_view(), name="comment-list-create"),
+    path("comments/<int:pk>/", CommentDetailView.as_view(), name="comment-detail"),
+
 ]
 
