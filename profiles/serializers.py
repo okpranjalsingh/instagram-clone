7ba8073d6463dc61b0ac0e9bda99ad3dd4d9c6f3
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-09 18:57:15 +0530

    updated/polished the code

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
-from .models import Profile, Follow
+from users.models import Follow
+from .models import Profile
 from django.contrib.auth import get_user_model
 
 class ProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+    profile_pic = serializers.ImageField(source='profile_pic', read_only=True)
+
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ['id', 'bio', 'profile_pic', 'followers_count', 'following_count', 'dob', 'gender']
 
+    def get_followers_count(self, obj):
+        return obj.followers.count()
 
+    def get_following_count(self, obj):
+        return obj.user.following.count()
 
 User = get_user_model()
 class ProfileSerializer(serializers.ModelSerializer):
