488029109058399df4de64877542bf60f154398d
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-16 20:24:03 +0530

    dm models

--- instadm/models.py
+++ instadm/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Dm(models.Model):
     username = models.ForeignKey(on_delete=models.CASCADE)
     textings = models.TextField(blank=False, null=False)
+    image = models.ImageField(blank=True, null=True)
+
 
 
     
