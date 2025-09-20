7c396c05f53c4004f2df49bd5c9889a51a255a0e
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-20 18:57:01 +0530

    refined models

--- instadm/models.py
+++ instadm/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Dm(models.Model):
-    username = models.ForeignKey(on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, blank=False, unique=True, null=False)
     textings = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
+
 
     def __str__(self):
         return self.username
     
     
-
     
 
