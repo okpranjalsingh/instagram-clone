03e3b9a7fa2ec727649ea52a4aeb8ebeefeac7a4
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-21 22:47:36 +0530

    random

--- instadm/models.py
+++ instadm/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Dm(models.Model):
     username = models.CharField(max_length=30, blank=False, unique=True, null=False)
     textings = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
+
 
 
     def __str__(self):
