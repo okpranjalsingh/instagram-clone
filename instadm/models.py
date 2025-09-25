a6061c40d3d455072b55e4d7c06b8245b49d65e0
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-25 18:23:26 +0530

    random commit

--- instadm/models.py
+++ instadm/models.py
@@ -7,6 +7,8 @@ class Dm(models.Model):
     username = models.CharField(max_length=30, blank=False, unique=True, null=False)
     textings = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
+    sender = models.CharField(max_length=30, blank=False, null=False})
 
     def __str__(self):
         return self.username
+    
