a9885bc70a16166df034864e6ad7b7ac969e06ab
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-30 21:45:29 +0530

    basic models changese

--- instadm/models.py
+++ instadm/models.py
@@ -7,8 +7,11 @@ class Dm(models.Model):
     username = models.CharField(max_length=30, blank=False, unique=True, null=False)
     textings = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
-    sender = models.CharField(max_length=30, blank=False, null=False})
+    sender = models.CharField(max_length=30, blank=False, null=False)
 
     def __str__(self):
         return self.username, self.sender
+
+class Info(models.Model):
+    pass
     
